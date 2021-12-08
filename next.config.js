06c953284f5b0a1aa0ecf2e7ab60d99eca3de056
Nam Dev <namdevdk2710@gmail.com>
2021-12-08 15:37:36 +0700

    Day 4

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-    images: {
-        domains: ["links.papareact.com"]
-    }
-}
+  images: {
+    domains: ['links.papareact.com'],
+  },
+  env: {
+    mapbox_key:
+      'pk.eyJ1IjoibmFtZGV2ZGsyNzEwIiwiYSI6ImNrd3g3cWVpMTBhZnUyeGxhdHhuM2Q3NHAifQ.4b5fYZn0RetLrMFTwpwbTw',
+  },
+};
